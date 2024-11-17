3ae84c90998da499439038a0727b14247f95236a
Эдуард <khidiyatov2000@bk.ru>
2024-11-17 11:52:56 +0500

    Maven и Spring Boot

--- src/main/java/ru/skypro/first_spring_application/service/CalculatorServiceImpl.java
+++ src/main/java/ru/skypro/first_spring_application/service/CalculatorServiceImpl.java
@@ -8,6 +8,9 @@ import ru.skypro.first_spring_application.service.CalculatorService;
 
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
+    public CalculatorServiceImpl() {
+    }
+
     @Override
     public int plus(int num1, int num2) {
         return num1 + num2;
@@ -32,49 +35,31 @@ public class CalculatorServiceImpl implements CalculatorService {
     @GetMapping("plus")
     public String plus(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
         if (num1 == null || num2 == null) {
+
+        }
             return "Оба аргумента обязательны";
         }
-
-        int result = calculatorService.plus(num1, num2);
-        return num1 + " + " + num2 + " = " + result;
-    }
 
 
     @GetMapping("minus")
     public String minus(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
         if (num1 == null || num2 == null) {
+
+        }
             return "Оба аргумента обязательны";
         }
-
-        int result = calculatorService.minus(num1, num2);
-        return num1 + " - " + num2 + " = " + result;
-    }
-
     @GetMapping("multiply")
     public String multiply( @RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
         if (num1 == null || num2 == null) {
+
+        }
             return "Оба аргумента обязательны";
         }
-
-        int result = calculatorService.multiply(num1, num2);
-        return num1 + " * " + num2 + " = " + result;
-    }
     @GetMapping("divide")
     public String divide( @RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2){
         if (num1 == null || num2 == null) {
+
+        }
             return "Оба аргумента обязательны";
         }
-        if (num2 == 0) {
-            return "Второе число не должно равняться нулю!";
-
-        }
-
-        int result = calculatorService.divide(num1, num2);
-        return num1 + " / " + num2 + " = " + result;
-
-
-
     }
-    }
-
-
